1cb73178dedc03fffbbd26d9662ac6f0b14670d9
Pavel <pavel.lorenc@gmail.com>
2022-04-26 09:21:44 +0200

    working insert do mongo db

---
+++ grpc_logger_services/mongo_db/mongo_db_services.py
@@ -0,0 +1,19 @@
+# Writes log data to Mongo DB using
+
+import asyncio, motor
+from beanie import  init_beanie
+from mongo_db.mongo_db_models import LogsDocument
+
+    
+# Beanie is fully asynchronous, so we will access it from an async function
+async def init_db():
+    # Beanie uses Motor under the hood 
+    #TODO Here use values from configuration
+    client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
+ 
+    # Init beanie with the Product document class
+    await init_beanie(database=client.logs, document_models=[LogsDocument])
+
+
+# if __name__ == '__main__':
+#     asyncio.run(init_db())
